e5988ac082b9e09e4cda93a227fc999570000c6c
Luka <lvukovic@veleri.hr>
2019-03-28 18:17:54 +0100

    CubeGame

---
+++ New Unity Project/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovment movement;
+    
+
+     void OnCollisionEnter(Collision collision)
+    {
+
+        if(collision.collider.tag == "Cube")
+        {
+            movement.enabled = false;
+
+
+            FindObjectOfType<GameManager>().EndGame();
+        }
+        //FindObjectOfType<GameManager>().EndGame();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerM : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float forwardForce = 1500f;
+    public float leftForce = -500f;
+    public float rightForce = 500f;
+    public float upForce = 2000f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(rightForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(leftForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, upForce * Time.deltaTime, 0);
+        }
+
+
+
+    }
+}
